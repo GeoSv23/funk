8d6f90076c81a6a4190aa5b0a167d83230d7e1ec
GeoSv23 <115006657+GeoSv23@users.noreply.github.com>
2023-10-03 23:32:57 +0300

    a

--- cards.js
+++ cards.js
@@ -1,41 +1,93 @@
-// отображение кнопок product
-const product__buttons = document.querySelector('.product__buttons')
+// burger 
+const buttonBurger = document.querySelector('.burger');
 
-const buttons = [
-  {title : 'свадьба'},
-  {title : 'мужу'},
-  {title : 'жене'},
-  {title : 'партнеру'},
-  {title : 'колекции'},
-  {title : 'редкость'},
+const burgerMenu = document.querySelector('.burgerMenu');
 
+const body = document.querySelector('body');
+
+const headerLogo = document.querySelector('.header__image');
+
+buttonBurger.addEventListener('click', function() {
+  burgerMenu.classList.toggle('burgerMenuOpen')
+  buttonBurger.classList.toggle('burgerActive')
+  body.classList.toggle('antiscroll')
+  headerLogo.classList.toggle('logoBurger')
+}
+)
+
+// 
+const socMedImg = document.querySelector('.socialMedias__images');
+
+const socMedAttributes = [
+  {link: '#', imgSrc: 'public/images/img--hover.png', alt: 'image'},
+  {link: '#', imgSrc: 'public/images/gallery__image.png', alt: 'image'},
+  {link: '#', imgSrc: 'public/images/gallery__image (1).png', alt: 'image'},
+  {link: '#', imgSrc: 'public/images/gallery__image (2).png', alt: 'image'},
+  {link: '#', imgSrc: 'public/images/gallery__image (3).png', alt: 'image'},
+  {link: '#', imgSrc: 'public/images/Rectangle 8.png', alt: 'image'},
 ]
 
+socMedAttributes.forEach((item) => {
+  const newImageContain = document.createElement('a');
+  newImageContain.classList.add('.socialMedias__images_container');
+})
 
-buttons.forEach(object => {
-  const newButton = document.createElement('button')
-  newButton.classList.add('product__buttons-item')
-  newButton.textContent = object.title
-  product__buttons.append(newButton)
+
+
+// отображение кнопок product
+const product__buttons = document.querySelector(".product__buttons");
+
+const buttons = [
+  { title: "свадьба" },
+  { title: "мужу" },
+  { title: "жене" },
+  { title: "партнеру" },
+  { title: "колекции" },
+  { title: "редкость" },
+];
+
+buttons.forEach((object) => {
+  const newButton = document.createElement("button");
+  newButton.classList.add("product__buttons-item");
+  newButton.textContent = object.title;
+  product__buttons.append(newButton);
 });
 
 console.log(product__buttons);
 
-
 // отображение картинок product
-const productImgHolder = document.querySelector('.product__images')
+const productImgHolder = document.querySelector(".product__images");
 
 const productCards = [
-  { cardLink: '1' ,imgSrc : 'public/images/Rectangle 5 (3).png', title: 'кольца'},
-  { cardLink: '2' ,imgSrc : 'public/images/Rectangle 5 (3).png', title: 'серьги'},
-  { cardLink: '3' ,imgSrc : 'public/images/Rectangle 5 (4).png', title: 'подвески'},
-  { cardLink: '4' ,imgSrc : 'public/images/Rectangle 5 (5).png', title: 'запонки'},
-  { cardLink: '5' ,imgSrc : 'public/images/Rectangle 5 (6).png', title: 'браслеты'},
-  { cardLink: '6' ,imgSrc : 'public/images/Rectangle 5.png', title: 'часы'},
+  {
+    cardLink: "1",
+    imgSrc: "public/images/Rectangle 5 (3).png",
+    title: "кольца",
+  },
+  {
+    cardLink: "2",
+    imgSrc: "public/images/Rectangle 5 (3).png",
+    title: "серьги",
+  },
+  {
+    cardLink: "3",
+    imgSrc: "public/images/Rectangle 5 (4).png",
+    title: "подвески",
+  },
+  {
+    cardLink: "4",
+    imgSrc: "public/images/Rectangle 5 (5).png",
+    title: "запонки",
+  },
+  {
+    cardLink: "5",
+    imgSrc: "public/images/Rectangle 5 (6).png",
+    title: "браслеты",
+  },
+  { cardLink: "6", imgSrc: "public/images/Rectangle 5.png", title: "часы" },
+];
 
-]
-
-productCards.forEach(object => {
+productCards.forEach((object) => {
   const newImage = `
   <a class="product__images_container" href="${object.cardLink}">
     <img src="${object.imgSrc}" alt="" />
@@ -46,14 +98,28 @@ productCards.forEach(object => {
 });
 
 // картинки
-const artImgHolder = document.querySelector('.usefoolArticles__images');
+const artImgHolder = document.querySelector(".usefoolArticles__images");
 
 const artCards = [
-  {artLink : '1', artImgSrc : 'public/images/Rectangle 5 (1).png', artAlt : '#', title : 'Как выбрать<br/> часы для своей<br/> будущей жены'},
-  {artLink : '2', artImgSrc : 'public/images/Rectangle 5 (7).png', artAlt : '#', title : 'Запонки для мужа:<br/> 7 ключевых правил<br/> покупки аксессуара'},
-  {artLink : '3', artImgSrc : 'public/images/Rectangle 5 (8).png', artAlt : '#', title : 'Как выбрать<br/> обручальные кольца<br/> молодоженам'},
-
-]
+  {
+    artLink: "1",
+    artImgSrc: "public/images/Rectangle 5 (1).png",
+    artAlt: "#",
+    title: "Как выбрать<br/> часы для своей<br/> будущей жены",
+  },
+  {
+    artLink: "2",
+    artImgSrc: "public/images/Rectangle 5 (7).png",
+    artAlt: "#",
+    title: "Запонки для мужа:<br/> 7 ключевых правил<br/> покупки аксессуара",
+  },
+  {
+    artLink: "3",
+    artImgSrc: "public/images/Rectangle 5 (8).png",
+    artAlt: "#",
+    title: "Как выбрать<br/> обручальные кольца<br/> молодоженам",
+  },
+];
 
 artCards.forEach((item) => {
   const newArtCard = `
@@ -63,8 +129,6 @@ artCards.forEach((item) => {
     </a>`;
 
   artImgHolder.innerHTML += newArtCard;
-
 });
 
-          
-          
+
